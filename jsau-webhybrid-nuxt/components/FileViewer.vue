a0132ab36d5ff8e2ba75d9277774df9b1a4ce331
Myriam Milha <myriam.milha@edu.univ-paris13.fr>
2026-01-18 20:48:11 +0100

    Add reusable FileViewer component

---
+++ jsau-webhybrid-nuxt/components/FileViewer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-html="content" class="html-container"></div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    content: String
+  });
+  </script>
+  
+  <style scoped>
+  .html-container {
+    border: 1px solid #ddd;
+    padding: 10px;
+    background: #fff;
+  }
+  </style>
+  
